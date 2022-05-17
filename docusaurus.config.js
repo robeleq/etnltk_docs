ee2e03a34e83786292192190af9f5a82fe757081
Robel Equbasilassie <robiki4life@gmail.com>
2022-05-17 09:10:18 +0300

    Project renamed to etnltk

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ETLTK',
-  tagline: 'Ethiopian Language Toolkit (etltk)',
-  url: 'https://etltk.netlify.app/',
+  title: 'ETNLTK',
+  tagline: 'Ethiopian Natural Language Toolkit (etnltk)',
+  url: 'https://etnltk.netlify.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'robikieq', // Usually your GitHub org/user name.
-  projectName: 'etltk', // Usually your repo name.
+  projectName: 'etnltk', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -38,14 +38,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/robikieq/etltk_docs/tree/main/',
+            'https://github.com/robikieq/etnltk_docs/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/robikieq/etltk_docs/tree/main/',
+            'https://github.com/robikieq/etnltk_docs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'ETLTK',
+        title: 'ETNLTK',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -68,7 +68,7 @@ const config = {
             type: 'docsVersionDropdown',
           },
           {
-            href: 'https://github.com/robikieq/ethiopian_language_toolkit/',
+            href: 'https://github.com/robikieq/etnltk/',
             label: 'GitHub',
             position: 'right',
           }/*
